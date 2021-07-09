c5919c85f0012875892cbb139f6186cefc11e708
khg0343 <sally0343@gmail.com>
2021-07-09 19:58:33 +0900

    Feat : NFC Function making

--- app/src/main/java/com/example/madcampproject2/MainActivity.java
+++ app/src/main/java/com/example/madcampproject2/MainActivity.java
@@ -2,21 +2,125 @@ package com.example.madcampproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.example.madcampproject2.ui.login.LoginActivity;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+
+import kotlin.jvm.internal.Intrinsics;
+
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "MainActivity";
+    private PendingIntent nfcPendingIntent;
+    private NfcAdapter nfcAdapter;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_main);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, LoginActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    }
 
-        Log.e("hello", "hello");
-        Log.e("hello", "hyeonji");
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, null, null);
+    }
 
-        Log.e("hello", "hihihihihihihihi");
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+//
+//    @Override
+//    protected void onNewIntent(Intent intent) {
+//        super.onNewIntent(intent);
+//
+//        Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+//        String writeValue = "http://www.naver.com";
+//        NdefMessage message = createTagMessage(writeValue);
+//
+//        writeTag(message, detectedTag);
+//
+//    }
 
-        Log.e("hello", "gaeun");
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent.getAction().equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
+            Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (messages == null) {
+                return;
+            }
+
+            for (Parcelable message : messages) {
+                showMsg((NdefMessage) message);
+            }
+        }
+
+    }
+
+    public void showMsg(NdefMessage mMessage) {
+        NdefRecord[] recs = mMessage.getRecords();
+
+        for (NdefRecord record : recs) {
+            if (Arrays.equals(record.getType(), NdefRecord.RTD_URI)) {
+
+                Uri u = record.toUri();
+
+                Intent j = new Intent(Intent.ACTION_VIEW);
+                j.setData(u);
+                startActivity(j);
+                finish();
+            }
+        }
+    }
+
+    private NdefMessage createTagMessage(String msg) {
+        return new NdefMessage(NdefRecord.createUri(msg));
+    }
+
+    public final void writeTag(NdefMessage message, Tag tag) {
+        int size = message.toByteArray().length;
+
+        try {
+            Ndef ndef = Ndef.get(tag);
+            if (ndef != null) {
+                ndef.connect();
+                if (!ndef.isWritable()) {
+                    Toast.makeText(this.getApplicationContext(), (CharSequence)"can not write NFC tag", Toast.LENGTH_SHORT).show();
+                }
+
+                if (ndef.getMaxSize() < size) {
+                    Toast.makeText(this.getApplicationContext(), (CharSequence)"NFC tag size too large", Toast.LENGTH_SHORT).show();
+                }
+
+                ndef.writeNdefMessage(message);
+                Toast.makeText(this.getApplicationContext(), (CharSequence)"NFC tag is writted", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Log.i(TAG,"error");
+        }
+
     }
 }
