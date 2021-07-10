fc8aefff7b439fd3842041e2a631ddc6f5d95d95
GAEUNYIM <59522019+GAEUNYIM@users.noreply.github.com>
2021-07-10 15:45:17 +0900

    kakao login complete - app

--- app/src/main/java/com/example/madcampproject2/MainActivity.java
+++ app/src/main/java/com/example/madcampproject2/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.net.Uri;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
@@ -11,11 +14,16 @@ import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.madcampproject2.ui.login.LoginActivity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 
@@ -23,12 +31,25 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnNfcRead;
     private Button btnNfcWrite;
+    private Button btnLogin;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
 
+        getAppKeyHash();
+
         btnNfcRead = findViewById(R.id.btn_nfc_read);
         btnNfcWrite = findViewById(R.id.btn_nfc_write);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO : click event
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+
+        });
 
         btnNfcRead.setOnClickListener(new Button.OnClickListener() {
             @Override
@@ -46,6 +67,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("name not found", e.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
 }
