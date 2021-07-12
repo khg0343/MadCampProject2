bd1c84bde282c91c9b0837605e86284ce461242e
GAEUNYIM <59522019+GAEUNYIM@users.noreply.github.com>
2021-07-13 02:08:24 +0900

    logout on lobby & ResultActivity

--- app/src/main/java/com/example/madcampproject2/LobbyActivity.java
+++ app/src/main/java/com/example/madcampproject2/LobbyActivity.java
@@ -3,16 +3,21 @@ package com.example.madcampproject2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
 
 
 public class LobbyActivity extends AppCompatActivity {
 
     private FloatingActionButton btnShowMap;
+    private Button btnLogout;
     private TextView txtLoginUserInfo;
 
     @Override
@@ -21,11 +26,26 @@ public class LobbyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lobby);
 
         btnShowMap = findViewById(R.id.btn_show_map);
+        btnLogout = findViewById(R.id.btn_logout_on_lobby);
         btnShowMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MapActivity.class);
                 startActivity(intent);
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserManagement.getInstance()
+                        .requestLogout(new LogoutResponseCallback() {
+                            @Override
+                            public void onCompleteLogout() {
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                Toast.makeText(LobbyActivity.this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
         String loginUserInfo;
@@ -37,7 +57,6 @@ public class LobbyActivity extends AppCompatActivity {
         else {
             loginUserInfo = "null";
         }
-
 
         txtLoginUserInfo = findViewById(R.id.txt_login_user_info);
         txtLoginUserInfo.setText(loginUserInfo);
--- app/src/main/java/com/example/madcampproject2/ResultActivity.java
+++ app/src/main/java/com/example/madcampproject2/ResultActivity.java
@@ -6,9 +6,39 @@ import android.os.Bundle;
 
 public class ResultActivity extends AppCompatActivity {
 
+    private double userLatitude = LoginResult.getLoginUser().getLatitude();
+    private double userLongitude = LoginResult.getLoginUser().getLongitude();
+
+    private double connectUserLatitude = LoginResult.getConnectUser().getLatitude();
+    private double connectUserLongitude = LoginResult.getConnectUser().getLongitude();
+
+    private double ourLatitude = ;
+    private double ourLongitude = ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+
     }
+
+    public double getOurDistance() { // 우리의 시작 시점 거리 구하기
+        double squareLatitude = Math.pow((userLatitude - connectUserLatitude), 2);
+        double squareLongitude = Math.pow((userLongitude - connectUserLongitude), 2);
+        return Math.pow((squareLatitude + squareLongitude), 0.5);
+    }
+
+    public double getUserTravel() { // 내가 이동해 온 거리 구하기
+        double squareLatitude = Math.pow((userLatitude - ourLatitude), 2);
+        double squareLongitude = Math.pow((userLongitude - ourLongitude), 2);
+        return Math.pow((squareLatitude + squareLongitude), 0.5);
+    }
+
+    public double getConnectUserTravel() { // 상대방이 이동해 온 거리 구하기
+        double squareLatitude = Math.pow((connectUserLatitude - ourLatitude), 2);
+        double squareLongitude = Math.pow((connectUserLongitude - ourLongitude), 2);
+        return Math.pow((squareLatitude + squareLongitude), 0.5);
+    }
+
 }
