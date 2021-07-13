12c2875671abd476825369923abd6b35b20ff30e
khg0343 <sally0343@gmail.com>
2021-07-14 01:53:48 +0900

    Feat: ResultActivity Finished!!!!!!!!!!!!!

--- app/src/main/java/com/example/madcampproject2/ResultActivity.java
+++ app/src/main/java/com/example/madcampproject2/ResultActivity.java
@@ -2,6 +2,7 @@ package com.example.madcampproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ViewGroup;
@@ -29,102 +30,111 @@ public class ResultActivity extends AppCompatActivity {
     private TextView txtConnectUserDistance;
     private TextView txtConnectUserScore;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-//        mapView = new MapView(this);
-//        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.resultMapView);
-//        mapViewContainer.addView(mapView);
+        mapView = new MapView(ResultActivity.this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.resultMapView);
+        mapViewContainer.addView(mapView);
+
 
         // 중심점 변경
-//        gpsTracker = new GpsTracker(this);
+        gpsTracker = new GpsTracker(this);
 //
-//        double latitude = gpsTracker.getLatitude(); double longitude = gpsTracker.getLongitude();
-//        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude), true);
-//        mapView.setZoomLevel(7, true); // 줌 레벨 변경
-//        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(latitude, longitude), 3, true); // 중심점 변경 + 줌 레벨 변경 (줌 레벨 숫자가 작을수록 확대되어 보임)
+        double latitude = gpsTracker.getLatitude(); double longitude = gpsTracker.getLongitude();
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude), true);
+        mapView.setZoomLevel(7, true); // 줌 레벨 변경
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(latitude, longitude), 3, true); // 중심점 변경 + 줌 레벨 변경 (줌 레벨 숫자가 작을수록 확대되어 보임)
+
+        mapView.removeAllPOIItems();
+        mapView.setCurrentLocationRadius(0);
+
+        txtLoginUserName = findViewById(R.id.txt_login_user_name_result);
+        txtLoginUserDistance = findViewById(R.id.txt_login_user_distance_result);
+        txtLoginUserScore = findViewById(R.id.txt_login_user_score_result);
+
+        txtConnectUserName = findViewById(R.id.txt_connect_user_name_result);
+        txtConnectUserDistance = findViewById(R.id.txt_connect_user_distance_result);
+        txtConnectUserScore = findViewById(R.id.txt_connect_user_score_result);
+//
+//
+        if(LoginResult.getLoginUser().getEmail() != null) {
+            distLoginUser = distance(
+                    LoginResult.getConnectLatitude(),
+                    LoginResult.getConnectLongitude(),
+                    LoginResult.getLoginUser().getLatitude(),
+                    LoginResult.getLoginUser().getLongitude(),
+                    "meter");
+
+            MapPOIItem marker = new MapPOIItem();
+            marker.setItemName(LoginResult.getLoginUser().getName());
+            marker.setMapPoint(MapPoint.mapPointWithGeoCoord(LoginResult.getLoginUser().getLatitude(), LoginResult.getLoginUser().getLongitude()));
+            marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+            mapView.addPOIItem(marker);
+
+        }
+        if(LoginResult.getConnectUser().getEmail() != null) {
+            distConnectUser = distance(
+                    LoginResult.getConnectLatitude(),
+                    LoginResult.getConnectLongitude(),
+                    LoginResult.getConnectUser().getLatitude(),
+                    LoginResult.getConnectUser().getLongitude(),
+                    "meter");
+
+            MapPOIItem marker = new MapPOIItem();
+            marker.setItemName(LoginResult.getConnectUser().getName());
+            marker.setMapPoint(MapPoint.mapPointWithGeoCoord(LoginResult.getConnectUser().getLatitude(), LoginResult.getConnectUser().getLongitude()));
+            marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+            mapView.addPOIItem(marker);
+        }
+//
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Connect");
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(LoginResult.getConnectLatitude(), LoginResult.getConnectLongitude()));
+        marker.setMarkerType(MapPOIItem.MarkerType.RedPin); // 기본으로 제공하는 RedPin 마커 모양.
+        mapView.addPOIItem(marker);
 
 
-//        txtLoginUserName = findViewById(R.id.txt_login_user_name_result);
-//        txtLoginUserDistance = findViewById(R.id.txt_login_user_distance_result);
-//        txtLoginUserScore = findViewById(R.id.txt_login_user_score_result);
-//
-//        txtConnectUserName = findViewById(R.id.txt_connect_user_name_result);
-//        txtConnectUserDistance = findViewById(R.id.txt_connect_user_distance_result);
-//        txtConnectUserScore = findViewById(R.id.txt_connect_user_score_result);
-//
-//
-//        if(LoginResult.getLoginUser().getEmail() != null) {
-//            distLoginUser = distance(
-//                    LoginResult.getConnectLatitude(),
-//                    LoginResult.getConnectLongitude(),
-//                    LoginResult.getLoginUser().getLatitude(),
-//                    LoginResult.getLoginUser().getLongitude(),
-//                    "meter");
-//
-////            MapPOIItem marker = new MapPOIItem();
-////            marker.setItemName(LoginResult.getLoginUser().getName());
-////            marker.setMapPoint(MapPoint.mapPointWithGeoCoord(LoginResult.getLoginUser().getLatitude(), LoginResult.getLoginUser().getLongitude()));
-////            marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
-//
-//        }
-//        if(LoginResult.getConnectUser().getEmail() != null) {
-//            distConnectUser = distance(
-//                    LoginResult.getConnectLatitude(),
-//                    LoginResult.getConnectLongitude(),
-//                    LoginResult.getConnectUser().getLatitude(),
-//                    LoginResult.getConnectUser().getLongitude(),
-//                    "meter");
-//
-////            MapPOIItem marker = new MapPOIItem();
-////            marker.setItemName(LoginResult.getConnectUser().getName());
-////            marker.setMapPoint(MapPoint.mapPointWithGeoCoord(LoginResult.getConnectUser().getLatitude(), LoginResult.getConnectUser().getLongitude()));
-////            marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
-//        }
-//
-////        MapPOIItem marker = new MapPOIItem();
-////        marker.setItemName("Connect");
-////        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(LoginResult.getConnectLatitude(), LoginResult.getConnectLongitude()));
-////        marker.setMarkerType(MapPOIItem.MarkerType.RedPin); // 기본으로 제공하는 RedPin 마커 모양.
-//
-//        if(LoginResult.getConnectUser().getEmail() != null && LoginResult.getLoginUser().getEmail() != null) {
-//            if(distLoginUser > distConnectUser) {
-//
-//                txtLoginUserName.setText(LoginResult.getLoginUser().getName());
-//                txtLoginUserDistance.setText((int) Math.round(distLoginUser) + "m");
-//                txtLoginUserScore.setText((int)(Math.round(distLoginUser)+50));
-//
-//                txtConnectUserName.setText(LoginResult.getConnectUser().getName());
-//                txtConnectUserDistance.setText((int) Math.round(distConnectUser) + "m");
-//                txtConnectUserScore.setText((int)(Math.round(distConnectUser)));
-//
-//            } else if (distLoginUser < distConnectUser) {
-//
-//                txtLoginUserName.setText(LoginResult.getLoginUser().getName());
-//                txtLoginUserDistance.setText((int) Math.round(distLoginUser) + "m");
-//                txtLoginUserScore.setText((int)(Math.round(distLoginUser)));
-//
-//                txtConnectUserName.setText(LoginResult.getConnectUser().getName());
-//                txtConnectUserDistance.setText((int) Math.round(distConnectUser) + "m");
-//                txtConnectUserScore.setText((int)(Math.round(distConnectUser)+50));
-//
-//            } else {
-//
-//                txtLoginUserName.setText(LoginResult.getLoginUser().getName());
-//                txtLoginUserDistance.setText((int) Math.round(distLoginUser) + "m");
-//                txtLoginUserScore.setText((int)(Math.round(distLoginUser)+100));
-//
-//                txtConnectUserName.setText(LoginResult.getConnectUser().getName());
-//                txtConnectUserDistance.setText((int) Math.round(distConnectUser) + "m");
-//                txtConnectUserScore.setText((int)(Math.round(distConnectUser)+100));
-//
-//            }
-//
-//        }
-//        Log.e("Result Activity::", "OnResume out");
 
+        if(LoginResult.getConnectUser().getEmail() != null && LoginResult.getLoginUser().getEmail() != null) {
+
+            int distLoginUserInt = (int) Math.round(distLoginUser);
+            int distConnectUserInt = (int) Math.round(distConnectUser);
+            String distLoginUserStr = Integer.toString(distLoginUserInt) + "m";
+            String distConnectUserStr = Integer.toString(distConnectUserInt) + "m";
+
+            if(distLoginUserInt > distConnectUserInt) {
+                txtLoginUserName.setText(LoginResult.getLoginUser().getName());
+                txtLoginUserDistance.setText(distLoginUserStr);
+                txtLoginUserScore.setText(Integer.toString(distLoginUserInt + 50));
+
+                txtConnectUserName.setText(LoginResult.getConnectUser().getName());
+                txtConnectUserDistance.setText(distConnectUserStr);
+                txtConnectUserScore.setText(Integer.toString(distConnectUserInt));
+
+            } else if (distLoginUserInt < distConnectUserInt) {
+                txtLoginUserName.setText(LoginResult.getLoginUser().getName());
+                txtLoginUserDistance.setText(distLoginUserStr);
+                txtLoginUserScore.setText(Integer.toString(distLoginUserInt));
+
+                txtConnectUserName.setText(LoginResult.getConnectUser().getName());
+                txtConnectUserDistance.setText(distConnectUserStr);
+                txtConnectUserScore.setText(Integer.toString(distConnectUserInt + 50));
+
+            } else {
+                txtLoginUserName.setText(LoginResult.getLoginUser().getName());
+                txtLoginUserDistance.setText(distLoginUserStr);
+                txtLoginUserScore.setText(Integer.toString(distLoginUserInt + 100));
+
+                txtConnectUserName.setText(LoginResult.getConnectUser().getName());
+                txtConnectUserDistance.setText(distConnectUserStr);
+                txtConnectUserScore.setText(Integer.toString(distConnectUserInt + 100));
+            }
+        }
+        Log.e("Result Activity::", "OnResume out");
     }
 
     @Override
